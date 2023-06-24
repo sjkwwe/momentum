7b074404a4cd3f18d7887d0803d55ee35376b7bd
CherRyeon <39686146+sjkwwe@users.noreply.github.com>
2023-06-24 20:13:22 +0900

    API_KEY Protected

--- src/weather.js
+++ src/weather.js
@@ -1,7 +1,7 @@
 function onGeoOk(position) {
   const lat = position.coords.latitude;
   const lon = position.coords.longitude;
-  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEYS.openWeatherMapAPI}&units=metric`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEYS}&units=metric`;
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
