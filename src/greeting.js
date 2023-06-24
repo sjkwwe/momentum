4ef984716b3faf91802d45080cf906cf785846df
CherRyeon <39686146+sjkwwe@users.noreply.github.com>
2023-06-24 23:23:48 +0900

    change img and change css

--- src/greeting.js
+++ src/greeting.js
@@ -9,7 +9,6 @@ function onLoginSubmit(event) {
   loginForm.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value;
   localStorage.setItem(USERNAME_KEY, username);
-  console.log(username);
   paintGreetings(username);
 }
 
---
+++ src/keys.js
@@ -0,0 +1,3 @@
+const API_KEYS = {
+  openWeatherMapAPI: "6ae6f219ffa48ba467287e571e3a8ef8",
+};
